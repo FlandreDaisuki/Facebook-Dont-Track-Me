cb633ce53406e5e479715110d4dd8b2098bd56e1
FlandreDaisuki <vbnm123c@gmail.com>
2019-01-05 12:01:20 +0800

    Apply right-click-copy to all pages for Chrome

--- src/content.js
+++ src/content.js
@@ -1,33 +1,35 @@
 /* global hardClarifyURL */
 
-document.addEventListener('mousedown', (event) => {
-  const ta = event.target.closest('a');
+if (location.hostname.includes('facebook.com')) {
+  document.addEventListener('mousedown', (event) => {
+    const ta = event.target.closest('a');
 
-  if (!ta) {
-    return;
-  }
+    if (!ta) {
+      return;
+    }
 
-  const href = ta.getAttribute('href');
-  if (href) {
-    const good = hardClarifyURL(ta.href);
-    // console.info('ta[href]', ta.href, '→', good);
-    ta.href = good;
-  }
+    const href = ta.getAttribute('href');
+    if (href) {
+      const good = hardClarifyURL(ta.href);
+      // console.info('ta[href]', ta.href, '→', good);
+      ta.href = good;
+    }
 
-  const ajaxify = ta.getAttribute('ajaxify');
-  if (ajaxify) {
-    const good = hardClarifyURL(ajaxify).replace(location.origin, '');
-    // console.info('ta[ajaxify]', ajaxify, '→', good);
-    ta.setAttribute('ajaxify', good);
-  }
+    const ajaxify = ta.getAttribute('ajaxify');
+    if (ajaxify) {
+      const good = hardClarifyURL(ajaxify).replace(location.origin, '');
+      // console.info('ta[ajaxify]', ajaxify, '→', good);
+      ta.setAttribute('ajaxify', good);
+    }
 
-  const lynxUri = ta.dataset.lynxUri;
-  if (lynxUri) {
-    const good = hardClarifyURL(lynxUri);
-    // console.info('ta[data-lynx-uri]', lynxUri, '→', good);
-    ta.dataset.lynxUri = good;
-  }
-});
+    const lynxUri = ta.dataset.lynxUri;
+    if (lynxUri) {
+      const good = hardClarifyURL(lynxUri);
+      // console.info('ta[data-lynx-uri]', lynxUri, '→', good);
+      ta.dataset.lynxUri = good;
+    }
+  });
+}
 
 // Chrome
 if (navigator.userAgent.includes('Chrome')) {
