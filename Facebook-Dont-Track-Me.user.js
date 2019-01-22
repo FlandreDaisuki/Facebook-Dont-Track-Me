4b2f15c11389bc0d2641bfd6f62258bd0acc56ca
FlandreDaisuki <vbnm123c@gmail.com>
2019-01-22 14:03:04 +0800

    Update userscript to v1.4.0

--- Facebook-Dont-Track-Me.user.js
+++ Facebook-Dont-Track-Me.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook-Dont-Track-Me
 // @namespace    https://github.com/FlandreDaisuki
-// @version      1.0
+// @version      1.4.0
 // @description  Strip Facebook track parameters and clarify url
 // @description:zh-TW 移除 Facebook 追蹤參數及淨化網址欄
 // @author       FlandreDaisuki
@@ -9,61 +9,241 @@
 // @grant        none
 // @noframes
 // ==/UserScript==
+/* eslint-disable no-unused-vars */
 
-const useless = [
-  '__tn__',
-  '__xts__[0]',
-  'eid',
-  'ref',
-  'fref',
-  'hc_ref',
-  'notif_id',
-  'notif_t',
-  'dti',
-  'comment_tracking',
-  'tn-str',
+const DEBUG_MODE = false;
+
+const SOFT_USELESS = [
+  'fbclid', // 元兇
 ];
 
-const url = new URL(location.href);
+const SOFT_PATTERNS = [
+  /^utm_/, // google analysis
+];
 
-url.searchParams.delete('fbclid');
+const HARD_USELESS = [
+  'eid',        // 動態的發文者/粉專使用者連結
+  'dti',      // 不知道從來定位到個人頁面會出現
+  'dpr',      // 不知道從來定位到個人頁面會出現
+  'rc',      // 不知道從來定位到個人頁面會出現
+  'comment_tracking', // 留言連結
+  'tn-str',   // theater 模式的發文者連結
+  'extragetparams',
+  'lst', // 跟個人頁面有關
+  'epa', // 跟 hashtag 有關
+];
 
-if (location.hostname === 'www.facebook.com') {
-  for (const key of useless) {
-    url.searchParams.delete(key);
+const HARD_PATTERNS = [
+  /^hc_/,
+  /^ft\[/,
+  /^\w*ref/,
+  /^notif_/,
+  /^__(?!a$|adt$)/,
+  /^timeline_context_item_/,
+];
+
+// Thanks this bug...
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1508174
+function* keyIterator(obj) {
+  const itor = obj.keys ? obj.keys() : Object.keys(obj)[Symbol.iterator]();
+
+  while (1) {
+    const { done, value } = itor.next();
+    if (done) { return; }
+    yield value;
   }
 }
 
-if (location.href !== url.href) {
-  // 只能讓網址欄變漂亮，requests 還是會帶追蹤參數
-  // 要在 request 移除追蹤參數，請安裝 webextension 版本
-
-  // Just clarify url in address bar but the tracking parameters are still in requests
-  // The webextension version can remove them from requests
-  history.replaceState({}, document.title, url.href.replace(url.origin, ''));
+function Objectify(obj) {
+  const o = {};
+  for (const k of keyIterator(obj)) {
+    o[k] = obj.get ? obj.get(k) : obj[k];
+  }
+  return o;
 }
 
-// process <a> in www.facebook.com
-if (location.hostname === 'www.facebook.com') {
-  document.body.addEventListener('click', (event) => {
+/**
+ * New an URL object accepts cross browser href/url
+ *
+ * @param {String|URL} url
+ * @returns {URL} URLObject
+ */
+function newURL(url, base) {
+  try {
+    return new URL(url, base);
+  } catch (error) {
+    return new URL(url, document.baseURI);
+  }
+}
+
+function diffObjectKeys(_ref, _cmp) {
+  const ref = Objectify(_ref), cmp = Objectify(_cmp);
+  const both = new Set(), added = new Set(), striped = new Set();
+  const keys = new Set([...keyIterator(ref), ...keyIterator(cmp)]);
+  for (const key of keys) {
+    if (key in ref && key in cmp) {
+      both.add(key);
+    } else if (!(key in ref) && key in cmp) {
+      added.add(key);
+    } else {
+      striped.add(key);
+    }
+  }
+
+  return {
+    both: [...both],
+    added: [...added],
+    striped: [...striped],
+  };
+}
+
+function softClarifyURL(_url, base) {
+  return clarifyURL(_url, base);
+}
+
+function hardClarifyURL(_url, base) {
+
+  // No facebook redirect
+  const isFacebookRedirect = (l) => {
+    const u = newURL(l);
+    u.search = '';
+    return u.href === 'https://l.facebook.com/l.php';
+  };
+
+  if (isFacebookRedirect(_url)) {
+    const u = newURL(newURL(_url).searchParams.get('u'));
+    return softClarifyURL(u, base);
+  }
+
+  return clarifyURL(_url, base, { hard: true });
+}
+
+/**
+ * Strip useless search parameters
+ *
+ * @param {String|URL} url
+ * @returns {String} href
+ */
+function clarifyURL(url, base, options = {}) {
+  let u = newURL(url, base);
+  if (u.href.includes('#') && !u.hash) {
+    // <a href="#">
+    return '#';
+  }
+
+  const good = clarifyObject(u.searchParams, options);
+  const badKeys = [];
+  for (const key of keyIterator(u.searchParams)) {
+    if (!(key in good)) {
+      badKeys.push(key);
+    }
+  }
+
+  for (const key of badKeys) {
+    u.searchParams.delete(key);
+  }
+
+  return u.href;
+}
+
+function clarifyObject(bad, options) {
+  const useless = SOFT_USELESS.slice();
+  const patterns = SOFT_PATTERNS.slice();
+
+  if (options.hard) {
+    useless.push(...HARD_USELESS);
+    patterns.push(...HARD_PATTERNS);
+  }
+
+  const good = Objectify(bad);
+
+  for (const key of keyIterator(bad)) {
+    if (patterns.some((p) => p.test(key)) || useless.includes(key)) {
+      delete good[key];
+    }
+  }
+
+  return good;
+}
+
+function log$Object(bad, good, title = 'Object') {
+  if (!DEBUG_MODE) { return; }
+
+  const { both, striped, added } = diffObjectKeys(bad, good);
+
+  console.groupCollapsed(title);
+
+  console.log('lefted:');
+  both.forEach((k) => console.log(`  ${k}`));
+
+  console.log('striped:');
+  striped.forEach((k) => console.log(`  ${k}`));
+
+  console.log('added:');
+  added.forEach((k) => console.log(`  ${k}`));
+
+  console.groupEnd(title);
+}
+
+function log$URL(bad, good, title = '') {
+  if (!DEBUG_MODE) { return; }
+
+  const [b, g] = [newURL(bad), newURL(good)];
+  const groupName = title || ((url) => {
+    const u = newURL(url);
+    u.search = '';
+    return u.href;
+  })(g);
+
+  const { both, striped } = diffObjectKeys(b.searchParams, g.searchParams);
+
+  console.groupCollapsed(groupName);
+
+  console.log('lefted:');
+  both.forEach((k) => console.log(`  ${k}`));
+
+  console.log('striped:');
+  striped.forEach((k) => console.log(`  ${k}`));
+
+  console.groupEnd(groupName);
+}
+
+if (location.hostname.includes('facebook.com')) {
+  document.addEventListener('mousedown', (event) => {
     const ta = event.target.closest('a');
 
-    if (ta &&
-        ta.target !== '_blank' &&
-        !ta.classList.contains('see_more_link') &&
-        ta.getAttribute('role') !== 'button' &&
-        ta.getAttribute('href') !== '#' &&
-        ta.rel !== 'ignore' &&
-        ta.rel !== 'theater')
-    {
-      event.preventDefault();
-      const url = new URL(ta.href);
+    if (!ta) {
+      return;
+    }
 
-      for (const key of useless) {
-        url.searchParams.delete(key);
-      }
+    const href = ta.getAttribute('href');
+    if (href) {
+      const good = hardClarifyURL(ta.href);
+      log$URL(ta.href, good, 'ta[href]');
+      ta.href = good;
+    }
 
-      location.assign(url.href);
+    const ajaxify = ta.getAttribute('ajaxify');
+    if (ajaxify) {
+      const good = hardClarifyURL(ajaxify).replace(location.origin, '');
+      log$URL(ajaxify, good, 'ta[ajaxify]');
+      ta.setAttribute('ajaxify', good);
+    }
+
+    const lynxUri = ta.dataset.lynxUri;
+    if (lynxUri) {
+      const good = hardClarifyURL(lynxUri);
+      log$URL(lynxUri, good, 'ta[data-lynx-uri]');
+      ta.dataset.lynxUri = good;
     }
   });
 }
+
+document.addEventListener('readystatechange', () => {
+  const sp = new URLSearchParams(location.hash);
+  const options = location.hostname.includes('facebook.com') ? { hard: true } : null;
+  const goodSearch = new URLSearchParams(clarifyObject(sp, options)).toString();
+  const goodURL = newURL(location.href);
+  goodURL.search = goodSearch;
+  history.replaceState(null, document.title, goodURL);
+});
